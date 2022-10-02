2619144793bc716381d9b130b2fb5fbb93b5222b
Sujanya Srinath <sujanya@Sujanyas-MacBook-Air.local>
2022-10-02 14:05:11 -0700

    commit step 5

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const users = {
        }
     ]
  }
-
+//Step 4 - Get the full list of users
  app.get('/users', (req, res) => {
     const name = req.query.name;
     if (name != undefined){
@@ -49,6 +49,24 @@ const users = {
 
 const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+//Get User by specific ID
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
 }
 
 app.listen(port, () => {
